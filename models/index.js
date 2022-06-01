d51ca4299f3d9c878f730a946f4157082d28a0c3
ManoLo2ManoLo <manuel_canas@aol.com>
2022-06-01 13:23:06 -0400

    link models in index.js **may need some tweaking**

---
+++ models/index.js
@@ -0,0 +1,45 @@
+const User = require('./User');
+const Subject = require('./Subject');
+const Course = require('./Course');
+const Section = require('./Section');
+const Question = require('./Question')
+
+User.hasMany(Course, {
+    foreignKey: 'user_id'
+});
+
+Course.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Course.hasMany(Section, {
+    foreignKey: 'course_id',
+    onDelete: 'SET NULL'
+});
+
+Section.belongsTo(Course, {
+    foreignKey: 'course_id',
+    onDelete: 'SET NULL'
+});
+
+Section.hasMany(Question, {
+    foreignKey: 'section_id',
+    onDelete: 'SET NULL'
+});
+
+Question.belongsTo(Section, {
+    foreignKey: 'section_id',
+    onDelete: 'SET NULL'
+});
+
+Course.hasOne(Subject, {
+    foreignKey: 'subject_id',
+    onDelete: 'SET NULL'
+});
+
+Subject.belongsTo(Course, {
+    foreignKey: 'subject_id',
+    onDelete: 'SET NULL'
+})
+
+module.exports = { User, Subject, Course, Section, Question }
