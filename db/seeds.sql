4ba29ff836960bd345c7f721a5ad2fa2bda1a7f9
ManoLo2ManoLo <manuel_canas@aol.com>
2022-05-31 22:07:36 -0400

    added the subject model

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO subject
+    (title)
+VALUES
+    ('Arts and Humanities'), 
+    ('Language'), 
+    ('Math'), 
+    ('Science'),
+    ('Social Science'), 
+    ('Other')
---
+++ models/Subject.js
@@ -0,0 +1,30 @@
+const { Model, DataTypes, Sequelize } = require('sequelize');
+const sequelize = require('../config/connectoin');
+
+class Subject extends Model {}
+
+Subject.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+
+        title: {
+            type: Sequelize.ENUM('Arts and Humanities', 'Language', 'Math', 'Science', 'Social Science', 'Other'),
+            validate: {
+                unique: true
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'subject'
+    }
+);
+
+module.exports = Score;
