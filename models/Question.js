ea464b5a37e2319f2f17911301586e71d6aaf1d8
ManoLo2ManoLo <manuel_canas@aol.com>
2022-06-01 12:56:29 -0400

    created question model

---
+++ models/Question.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Question extends Model {}
+
+ Question.init(
+     {
+         id: {
+             type: DataTypes.INTEGER,
+             allowNull: false,
+             autoIncrement: true,
+             primaryKey: true
+         },
+
+         question: {
+             type: DataTypes.STRING,
+             allowNull: false
+         },
+
+         answer: {
+             type: DataTypes.STRING,
+             allowNull: false
+         }
+     },
+     {
+         sequelize,
+         freezeTableName: true,
+         underscored: truw,
+         modelName: 'question'
+     }
+ );
+
+ module.exports = Question;
