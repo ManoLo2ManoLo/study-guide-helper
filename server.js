581ae33b7ab5f6dbae2b5f0ef4c4e56f54ffcb74
ManoLo2ManoLo <manuel_canas@aol.com>
2022-06-02 00:47:07 -0400

    set up handlebars on server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -17,6 +18,11 @@ const sess = {
         db: sequelize
     })
 };
+
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
