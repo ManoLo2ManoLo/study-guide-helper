34347e60500f3c06943d324ebbf46d34c859c48c
ManoLo2ManoLo <manuel_canas@aol.com>
2022-06-02 01:50:48 -0400

    set up handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const hbsRoutes = require('./hbs');
 
 router.use('/api', apiRoutes);
+router.use('/', hbsRoutes);
 
 module.exports = router;
