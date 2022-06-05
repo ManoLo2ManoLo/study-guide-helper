fdbbb3bbb424195896b511ab2bd6bcce95a5b4c6
ManoLo2ManoLo <manuel_canas@aol.com>
2022-06-04 22:49:22 -0400

    created routes for login and signup pages

--- controllers/hbs/index.js
+++ controllers/hbs/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 const homeRoutes = require('./home');
+const loginRoutes = require('./login');
+const signupRoutes = require('./signup');
 
 router.use('/', homeRoutes);
+router.use('/login', loginRoutes);
+router.use('/signup', signupRoutes);
 
 module.exports = router;
