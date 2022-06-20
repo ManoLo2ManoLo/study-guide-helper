dd3f3a67bcfd5e037ec689f9ce2ebb8e4efa4d36
ManoLo2ManoLo <manuel_canas@aol.com>
2022-06-20 18:29:07 -0400

    courses and sections tabs are on side bars

--- controllers/hbs/dashboard/index.js
+++ controllers/hbs/dashboard/index.js
@@ -1,8 +1,45 @@
 const router = require('express').Router();
+const { User, Course, Subject, Section } = require('../../../models');
 const withAuth = require('../../../utils/auth.js');
 
 router.get('/', withAuth, (req, res) => {
-    res.render('dashboard');
-})
+    User.findOne({
+        where: {
+            id: req.session.user_id
+        },
+        attributes: {
+            exclude: ['password']
+        },
+        include: [
+            {
+                model: Course,
+                attributes: [ 'id', 'course_name' ],
+                include: [
+                    {
+                        model: Subject,
+                        attributes: [ 'id', 'subject_name' ]
+                    },
+                    {
+                        model: Section,
+                        attributes: [ 'id', 'section_name' ]
+                    }
+                ]
+            }
+        ]
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id' });
+        }
+
+        const user = dbUserData.get({ plain: true });
+
+        res.render('dashboard', {user});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
